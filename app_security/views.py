859bda6ed1a56f9e275fd1d813a8b5125db1872f
alguien180 <miguel_agj@protonmail.com>
2025-07-30 16:30:27 +0200

    Added to requirements all packages. updated gitignore

---
+++ app_security/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic.edit import FormView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
+
+class CustomLoginView(LoginView):
+    template_name = 'login.html'
+    fields='__all__'
+    redirect_authenticated_user=True
+    
+    def get_success_url(self):
+        return reverse_lazy('notes')
+
+class RegisterPage(FormView):
+    template_name = 'register.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user= True #this is overwritten with the def get
+    success_url= reverse_lazy('notes')
+
+    def form_valid(self,form):
+        user = form.save()
+        if user is not None:
+            login(self.request,user)
+        return super(RegisterPage,self).form_valid(form)
+    
+    def get(self,*args,**kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('notes')
+        return super(RegisterPage,self).get(*args,**kwargs)#overwriting the top fnuction because it did not stopped logged users from entering the create account
+git 
